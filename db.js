d3773ac62a21c0cfaf7f904fe4c1a6f169701688
Austin73 <maneeshpal85@gmail.com>
2022-03-02 21:18:12 +0530

    built initial architecture

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongoose= require('mongoose')
+const app= require('./index.js')
+const PORT= 5000;
+mongoose.connect(process.env.MONGOURI,{
+
+ useNewUrlParser:true,
+ useUnifiedTopology : true
+})
+.then(()=>{
+    console.log("Connected to DB");
+    app.listen(PORT,()=>{
+    console.log("server is running");
+
+})
+})
+.catch((err)=>{
+    console.log("error with connecting database");
+})
